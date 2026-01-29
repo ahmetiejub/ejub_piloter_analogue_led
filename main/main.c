655a45b33914f5615b44577115e41174f4f7d00b
ahmetiejub <ahmetiejub@gmail.com>
2026-01-29 19:51:59 +0100

    Ajout de tout le code.

---
+++ main/main.c
@@ -0,0 +1,169 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_wifi.h"
+#include "esp_system.h"
+#include "esp_event.h"
+#include "esp_event_loop.h"
+#include "nvs_flash.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include "server_task.h"
+#include "client_task.h"
+#include "wifi.h"
+#include "led.h"
+#include "device.h"
+
+#include "config.h"
+#include "address.h"
+#include "bacdef.h"
+#include "handlers.h"
+#include "client.h"
+#include "dlenv.h"
+#include "bacdcode.h"
+#include "npdu.h"
+#include "apdu.h"
+#include "iam.h"
+#include "tsm.h"
+#include "datalink.h"
+#include "dcc.h"
+#include "getevent.h"
+#include "net.h"
+#include "txbuf.h"
+#include "version.h"
+#include "av.h"
+#include "sdkconfig.h"
+
+#define SERVER_DEVICE_ID CONFIG_SERVER_DEVICE_ID
+
+static const char *TAG = "main";
+
+// definition of what functions we have
+static object_functions_t Object_Table[] = {
+   { 
+        OBJECT_DEVICE, NULL, /* don't init - recursive! */
+        Device_Count, 
+        Device_Index_To_Instance,                
+        Device_Valid_Object_Instance_Number,
+        Device_Object_Name, 
+        Device_Read_Property_Local,
+        Device_Write_Property_Local, 
+        Device_Property_Lists 
+    },
+    { 
+        OBJECT_ANALOG_VALUE, 
+        Analog_Value_Init, 
+        Analog_Value_Count,
+        Analog_Value_Index_To_Instance, 
+        Analog_Value_Valid_Instance,
+        Analog_Value_Object_Name, 
+        Analog_Value_Read_Property,
+        Analog_Value_Write_Property, 
+        Analog_Value_Property_Lists 
+    },
+};
+
+/** Initialize the handlers we will utilize.
+ * @see Device_Init, apdu_set_unconfirmed_handler, apdu_set_confirmed_handler
+ */
+static void Init_Service_Handlers(void)
+{
+    Device_Init(&Object_Table[0]);
+    /* we need to handle who-is to support dynamic device binding */
+    apdu_set_unconfirmed_handler(SERVICE_UNCONFIRMED_WHO_IS, handler_who_is);
+    apdu_set_unconfirmed_handler(SERVICE_UNCONFIRMED_WHO_HAS, handler_who_has);
+    /* handle i-am to support binding to other devices */
+    apdu_set_unconfirmed_handler(SERVICE_UNCONFIRMED_I_AM, handler_i_am_bind);
+    /* set the handler for all the services we don't implement */
+    /* It is required to send the proper reject message... */
+    apdu_set_unrecognized_service_handler_handler
+        (handler_unrecognized_service);
+    /* Set the handlers for any confirmed services that we support. */
+    /* We must implement read property - it's required! */
+    apdu_set_confirmed_handler(SERVICE_CONFIRMED_READ_PROPERTY,
+        handler_read_property);
+    apdu_set_confirmed_handler(SERVICE_CONFIRMED_READ_PROP_MULTIPLE,
+        handler_read_property_multiple);
+    apdu_set_confirmed_handler(SERVICE_CONFIRMED_WRITE_PROPERTY,
+        handler_write_property);
+    apdu_set_confirmed_handler(SERVICE_CONFIRMED_WRITE_PROP_MULTIPLE,
+        handler_write_property_multiple);
+    apdu_set_confirmed_handler(SERVICE_CONFIRMED_READ_RANGE,
+        handler_read_range);
+
+    apdu_set_confirmed_handler(SERVICE_CONFIRMED_REINITIALIZE_DEVICE,
+        handler_reinitialize_device);
+    apdu_set_unconfirmed_handler(SERVICE_UNCONFIRMED_UTC_TIME_SYNCHRONIZATION,
+        handler_timesync_utc);
+    apdu_set_unconfirmed_handler(SERVICE_UNCONFIRMED_TIME_SYNCHRONIZATION,
+        handler_timesync);
+    apdu_set_confirmed_handler(SERVICE_CONFIRMED_SUBSCRIBE_COV,
+        handler_cov_subscribe);
+    apdu_set_unconfirmed_handler(SERVICE_UNCONFIRMED_COV_NOTIFICATION,
+        handler_ucov_notification);
+    /* handle communication so we can shutup when asked */
+    apdu_set_confirmed_handler(SERVICE_CONFIRMED_DEVICE_COMMUNICATION_CONTROL,
+        handler_device_communication_control);
+    /* handle the data coming back from private requests */
+    apdu_set_unconfirmed_handler(SERVICE_UNCONFIRMED_PRIVATE_TRANSFER,
+        handler_unconfirmed_private_transfer);
+}
+
+void app_main(void)
+{
+    // Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    // Initialize networking stack
+    ESP_ERROR_CHECK(esp_netif_init());
+
+    // Initialize event loop
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    // Initialize wifi and connect
+    wifi_initialize();
+
+    // set up led
+    led_initialize();
+
+    // ======= MODIFICATION : Configuration GPIO 23 pour la LED =======
+    gpio_config_t io_conf = {
+        .pin_bit_mask = (1ULL << GPIO_NUM_23),
+        .mode = GPIO_MODE_OUTPUT,
+        .pull_up_en = GPIO_PULLUP_DISABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .intr_type = GPIO_INTR_DISABLE
+    };
+    gpio_config(&io_conf);
+    gpio_set_level(GPIO_NUM_23, 0);  // LED éteinte au démarrage
+    ESP_LOGI(TAG, "GPIO23 configured for LED control");
+    // ================================================================
+
+    /* allow the device ID to be set */
+    Device_Set_Object_Instance_Number(SERVER_DEVICE_ID);
+    printf("BACnet Server Demo\n" "BACnet Stack Version %s\n"
+        "BACnet Device ID: %lu\n" "Max APDU: %d\n", BACnet_Version,
+        (unsigned long)Device_Object_Instance_Number(), MAX_APDU);
+    /* load any static address bindings to show up
+       in our device bindings list */
+    address_init();
+    Init_Service_Handlers();
+    dlenv_init();
+    atexit(datalink_cleanup);
+    /* configure the timeout values */
+    /* broadcast an I-Am on startup */
+    Send_I_Am(&Handler_Transmit_Buffer[0]);
+
+    // start bacnet server
+    xTaskCreate(server_task,"bacnet_server", 8000, NULL, 1, NULL);
+
+    // start bacnet client
+    xTaskCreate(client_task,"bacnet_client", 8000, NULL, 1, NULL);
+
+    ESP_LOGI(TAG, "BACnet demo started");
+}
